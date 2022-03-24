68401ca18fbacb38e076cb8a9a1db9e3ae43a26c
Thomas shih <tt7919@gmail.com>
2022-03-24 17:10:06 +0800

    feat: update 命盤 structures

--- src/app/interfaces/命盤.ts
+++ src/app/interfaces/命盤.ts
@@ -1,8 +1,33 @@
+import { 命盤結果屬性 } from 'src/app/enums/命盤.enum';
 import { 地支 } from 'src/app/enums/地支.enum';
 import { 天干 } from 'src/app/enums/天干.enum';
 
 export interface 命盤 {
-  本命: 天干[] | 地支[];
-  大運: 天干 | 地支;
-  流年: 天干 | 地支;
+  天干: 天干命盤;
+  地支: 地支命盤;
+}
+
+export interface 命盤結果 {
+  [命盤結果屬性.時住已合]: boolean;
+  [命盤結果屬性.日住已合]: boolean;
+  [命盤結果屬性.月住已合]: boolean;
+  [命盤結果屬性.年住已合]: boolean;
+  [命盤結果屬性.大運已合]: boolean;
+  [命盤結果屬性.流年已合]: boolean;
+}
+
+export interface 天干命盤 {
+  命盤結果: 命盤結果;
+  本命: 天干[];
+  大運: 天干;
+  流年: 天干;
+  已合: 天干[][];
+}
+
+export interface 地支命盤 {
+  命盤結果: 命盤結果;
+  本命: 地支[];
+  大運: 地支;
+  流年: 地支;
+  已合: 地支[][];
 }
