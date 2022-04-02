4ead7001d3e107907a8da1b4bcffca7c36928af1
Thomas shih <tt7919@gmail.com>
2022-04-02 14:29:53 +0800

    refactor: remove console.log

--- src/app/services/命盤/命盤.service.spec.ts
+++ src/app/services/命盤/命盤.service.spec.ts
@@ -15,8 +15,6 @@ describe('命盤Service', () => {
     it('測試命盤結果 with case 1994/11/26 05:12', () => {
       const 測試天干命盤 = service.創建天干命盤(new Date('1994/11/26 05:13'), true);
 
-      console.log(測試天干命盤);
-
       expect(測試天干命盤.本命).toEqual([天干.辛, 天干.丙, 天干.乙, 天干.甲]);
     });
 
