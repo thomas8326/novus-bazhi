6c3048876db545875b7e1fe490f30bc39d19ff7f
Thomas shih <tt7919@gmail.com>
2022-04-02 22:31:34 +0800

    feat: update interface and json test structure.

--- src/app/services/算命/算命測試.ts
+++ src/app/services/算命/算命測試.ts
@@ -2,14 +2,26 @@ import { 地支 } from 'src/app/enums/地支.enum';
 import { 天干 } from 'src/app/enums/天干.enum';
 import { 命盤結果 } from 'src/app/interfaces/命盤';
 
-export interface 算命測試 {
-  天干測試: {
-    描述: string;
-    命盤: {
-      本命: 天干[] | 地支[];
-      大運: 天干 | 地支;
-      流年: 天干 | 地支;
-    };
-    預期: 命盤結果;
+export interface 天干測試 {
+  描述: string;
+  命盤: {
+    本命: 天干[];
+    大運: 天干;
+    流年: 天干;
   };
+  預期: 命盤結果;
+}
+
+export interface 地支測試 {
+  描述: string;
+  命盤: {
+    本命: 地支[];
+    大運: 地支;
+    流年: 地支;
+  };
+  預期: 命盤結果;
+}
+
+export interface 算命測試 {
+  天干測試: 天干測試[];
 }
