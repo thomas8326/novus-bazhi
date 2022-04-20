fdb170148b12000c8605cec51c1549128cbf2faf
Thomas shih <tt7919@gmail.com>
2022-04-20 15:52:40 +0800

    feat: add datepicker dependencys

--- src/app/member-board/member-board.module.ts
+++ src/app/member-board/member-board.module.ts
@@ -5,14 +5,17 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 import { MemberBoardComponent } from './member-board.component';
 import { MemberBoardRoutingModule } from './member-board-routing.module';
 import { MemberTableComponent } from './member-table/member-table.component';
 import { MemberControlPanelComponent } from './member-control-panel/member-control-panel.component';
+import { DobPickerComponent } from './dob-picker/dob-picker.component';
 
 @NgModule({
-  declarations: [MemberBoardComponent, MemberTableComponent, MemberControlPanelComponent],
+  declarations: [MemberBoardComponent, MemberTableComponent, MemberControlPanelComponent, DobPickerComponent],
   imports: [
     CommonModule,
     MemberBoardRoutingModule,
@@ -21,6 +24,8 @@ import { MemberControlPanelComponent } from './member-control-panel/member-contr
     ReactiveFormsModule,
     MatInputModule,
     MatRadioModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
   ],
 })
 export class MemberBoardModule {}
