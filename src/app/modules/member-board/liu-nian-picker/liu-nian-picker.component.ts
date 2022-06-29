d0c902d2714239dd50901d7b274f452dc239c4cd
Thomas shih <tt7919@gmail.com>
2022-06-29 23:45:55 +0800

    feat: add button to go previous or next page.

--- src/app/modules/member-board/liu-nian-picker/liu-nian-picker.component.ts
+++ src/app/modules/member-board/liu-nian-picker/liu-nian-picker.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { switchMap } from 'rxjs';
+import { MAX_YEAR_DISTANCE } from 'src/app/constants/constants';
 import { 命盤 } from 'src/app/interfaces/命盤';
 import { Member } from 'src/app/interfaces/會員';
 import { MemberService } from 'src/app/services/member/member.service';
@@ -17,8 +18,11 @@ export class LiuNianPickerComponent {
 
   member: Member | null = null;
   currentYear: number = new Date().getFullYear();
-
-  horoscopeList: 命盤[] = [];
+  atYear = 0;
+  currentIndex = 0;
+  minYear = 0;
+  maxYear = 0;
+  horoscopeList: 命盤[][] = [];
 
   constructor(
     private readonly router: Router,
@@ -31,11 +35,16 @@ export class LiuNianPickerComponent {
       if (member) {
         this.member = new Member(member);
         this.currentYear = isNaN(this.member.atYear) ? this.currentYear : this.member.atYear;
+        this.atYear = this.currentYear;
+        const horoscopes: 命盤[] = [];
         for (let i = this.currentYear - 1; i <= this.currentYear + 1; i++) {
-          this.horoscopeList.push(this.命盤分析(this.member, i));
+          horoscopes.push(this.命盤分析(this.member, i));
         }
-        this.命盤服務.新增命盤(this.member.id, this.horoscopeList);
+        this.horoscopeList.push(horoscopes);
+        this.命盤服務.新增命盤(this.member.id, horoscopes);
         this.memberService.setSelectedMembers(this.member);
+        this.minYear = this.member.getDobDate().getFullYear();
+        this.maxYear = this.minYear + MAX_YEAR_DISTANCE - 1;
       }
     });
   }
@@ -44,6 +53,47 @@ export class LiuNianPickerComponent {
     if (this.member) {
       this.router.navigate([`result`], { relativeTo: this.activatedRoute, queryParams: { year } });
     }
+  }
+
+  onPrevYear() {
+    if (!this.member) {
+      return;
+    }
+
+    this.currentIndex = this.currentIndex - 1;
+    this.currentYear = this.currentYear - 3;
+
+    if (this.currentIndex >= 0) {
+      return;
+    }
+
+    this.currentIndex = 0;
+    const horoscopes: 命盤[] = [];
+    for (let i = this.currentYear - 1; i <= this.currentYear + 1; i++) {
+      horoscopes.push(this.命盤分析(this.member, i));
+    }
+    this.horoscopeList.unshift(horoscopes);
+    this.命盤服務.新增命盤(this.member.id, horoscopes);
+  }
+
+  onNextYear() {
+    if (!this.member) {
+      return;
+    }
+
+    this.currentIndex = this.currentIndex + 1;
+    this.currentYear = this.currentYear + 3;
+
+    if (this.currentIndex < this.horoscopeList.length) {
+      return;
+    }
+
+    const horoscopes: 命盤[] = [];
+    for (let i = this.currentYear - 1; i <= this.currentYear + 1; i++) {
+      horoscopes.push(this.命盤分析(this.member, i));
+    }
+    this.horoscopeList.push(horoscopes);
+    this.命盤服務.新增命盤(this.member.id, horoscopes);
   }
 
   private 命盤分析(member: Member, year: number) {
